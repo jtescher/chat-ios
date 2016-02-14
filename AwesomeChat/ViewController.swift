67d4f3b9d97fd92b576335f1ac64f5e1e4149e9c
Julian Tescher <jatescher@gmail.com>
2016-02-14 11:35:54 -0800

    Add initial app scaffold

---
+++ AwesomeChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwesomeChat
+//
+//  Created by Julian Tescher on 2/14/16.
+//  Copyright © 2016 Botchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat iOS
+
+This project is a client app prototype for the [chat-node](https://github.com/jtescher/chat-node) server prototype.
